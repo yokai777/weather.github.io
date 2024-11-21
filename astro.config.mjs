f60fac6975c0e02407adee934702e306bf1275b4
Nichita777 <nichita.selchin@gmail.com>
2024-11-21 18:51:43 +0000

    3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github';
 
 export default defineConfig({
   output: 'static',
-  integrations: [github()],
-  base: '/', // Use '/' since it's a direct GitHub Pages repository
+  base: '/', // Use '/' for GitHub Pages unless your repository is under a subpath
 });
